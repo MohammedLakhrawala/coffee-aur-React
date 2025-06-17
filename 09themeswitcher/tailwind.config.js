21fcb5ffcb651e2fc4d75f072f62a558cf149924
MohammedLakhrawala <codewthmohammed@gmail.com>
2025-06-17 16:48:44 +0530

    context api method 2 for light nd dark theme

---
+++ 09themeswitcher/tailwind.config.js
@@ -0,0 +1,15 @@
+import { data } from 'autoprefixer';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
